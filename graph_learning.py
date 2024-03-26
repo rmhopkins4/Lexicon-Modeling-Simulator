24dd45527f253f98b5d624e02858e71df2e90962
Robert Hopkins <rmhopkins4@gmail.com>
2024-03-25 22:25:36 -0400

    many graphs can be made
    tbd: assign initial vocab

--- graph_learning.py
+++ graph_learning.py
@@ -1,42 +1,60 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-RANDOM_SEED = 118755212
+import random
+import pprint
+
+# RANDOM_SEED = 118755212
+# random.seed(RANDOM_SEED)
+NUM_SYMBOLS = 10
 
 
-def create_regular_graph_w_pop(n, k, num_symbols):
-    G = nx.Graph()
+def create_regular_graph_w_pop(nodes, neighbors, num_symbols=NUM_SYMBOLS):
+    G = nx.connected_watts_strogatz_graph(nodes, neighbors, 0)
 
-    # Add nodes
-    G.add_nodes_from(range(n))
-
-    # Connect each node to k neighbors (k/2 on each side)
-    for i in range(n):
-        for j in range(1, k // 2 + 1):
-            neighbor1 = (i + j) % n
-            neighbor2 = (i - j) % n
-            G.add_edge(i, neighbor1)
-            G.add_edge(i, neighbor2)
-
-    # return G
+    return G
 
 
-def create_lopsided_barbell_graph_w_pop(n, m, k, num_symbols):
-    # Create first complete
-    bell1 = nx.complete_graph(n)
-
-    for i in range(k):
-        bell1.add_node(n+i)
-        bell1.add_edge(n+i-1, n+i)
-
-    bell2 = nx.complete_graph(m)
+def create_2bell_bars_graph_w_pop(size_1, size_2, len_bar, num_bars, num_symbols=NUM_SYMBOLS):
+    # Create first completes
+    bell1 = nx.complete_graph(range(size_1))
+    bell2 = nx.complete_graph(range(size_1, size_1 + size_2))
 
     # Combine the two bells using disjoint_union
     barbell_graph = nx.disjoint_union(bell1, bell2)
 
-    barbell_graph.add_edge(n+k-1, n+k)
+    total_nodes = size_1 + size_2
+
+    # add bars
+    for i in range(num_bars):
+        prev_node = random.choice(range(size_1))
+        for j in range(len_bar):
+            barbell_graph.add_node(total_nodes + (len_bar * i) + j)
+            barbell_graph.add_edge(total_nodes + (len_bar * i) + j, prev_node)
+            prev_node = (total_nodes + (len_bar * i) + j)
+
+        barbell_graph.add_edge(prev_node,
+                               random.choice(range(size_1, size_1 + size_2)))
 
     return barbell_graph
+
+
+def create_bipartite_graph_w_pop(group_1, group_2, num_symbols=NUM_SYMBOLS):
+    G = nx.bipartite.complete_bipartite_graph(group_1, group_2)
+
+    return G
+
+
+def create_lollipop_graph_w_pop(candy_nodes, stick_nodes, num_symbols=NUM_SYMBOLS):
+    G = nx.lollipop_graph(candy_nodes, stick_nodes)
+
+    return G
+
+
+def create_small_world_graph_w_pop(nodes, neighbors, rewire_odds, num_symbols=NUM_SYMBOLS):
+    G = nx.connected_watts_strogatz_graph(nodes, neighbors, rewire_odds)
+
+    return G
 
 # random drift (together) after interactions? more impact on barbell-type graphs, accents
 #   depends on how i model current state, difficult to do w/ homesign method, which in all likelihood i will keep
@@ -50,11 +68,9 @@ def create_lopsided_barbell_graph_w_pop(n, m, k, num_symbols):
 
 # barabási–albert?
 
-# Function to create a regular graph with n nodes and k neighbors
 
-
-g = create_lopsided_barbell_graph_w_pop(10, 20, 0)
+g = create_small_world_graph_w_pop(10, 4, 0.1)
 a = nx.to_numpy_array(g)
-print(a)
-nx.draw(g, with_labels=True, node_color='skyblue', node_size=100)
+pprint.pp(a)
+nx.draw(g, with_labels=True, node_color='pink', node_size=100)
 plt.show()
